6aa466867cfc351b24d897cd3aa33e3ca16be71a
Zaham2 <zaham2@yahoo.com>
2024-05-04 11:43:06 +0300

    WIP: removed singleton supabase client

--- db/client.ts
+++ db/client.ts
@@ -1,5 +1,2 @@
-import { SupabaseClient } from '@supabase/supabase-js';
-import { createClient } from './config';
-
-const supabase: SupabaseClient = createClient();
-export default supabase
+// It came to my attention that the client is lightwaight and that its best to instantiate a client
+// for every request, rather than create a singleton client.
